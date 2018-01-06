00a7c4139a5740760260ace3689d022f1b6258d5
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-01-06 15:13:45 -0500

    Fixed Navbar and edited config.

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -57,7 +57,7 @@ materialKit = {
     },
 
     checkScrollForTransparentNavbar: debounce(function() {
-            if($(document).scrollTop() > 260 ) {
+            if($(document).scrollTop() > 100 ) {
                 if(transparent) {
                     transparent = false;
                     $('.navbar-color-on-scroll').removeClass('navbar-transparent');
